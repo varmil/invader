e80329d1c6ce4cb6e02b01b61bd3328ad9debbfa
Your Name <fkmks247@gmail.com>
2018-03-06 18:10:16 +0900

    Implement Enemy Basic Movement

---
+++ Assets/Resources/Script/EnemyCloud.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * EnemyZone内に存在する敵の塊
+ */
+public class EnemyCloud : MonoBehaviour {
+	// 列ごとの移動秒数差
+	private static readonly float MovingIntervalPerLine = 0.15f;
+	// １回の移動でどの程度X軸方向に動くか
+	private static readonly float MovingAmountX = 0.25f;
+	// 最も高い位置の初期Y座標
+	private static readonly float FirstLineYPos = 15f;
+
+	public List<Line> Lines {
+		get { return lines; }
+	}
+
+	public float RightEnd {
+		get {
+			Debug.Log ("Cloud RightEnd::" + Mathf.Max (Lines.Select ((e) => e.RightEnd).ToArray()).ToString());
+			return Mathf.Max (Lines.Select ((e) => e.RightEnd).ToArray());
+		}
+	}
+
+	public float LeftEnd {
+		get { 
+			Debug.Log ("Cloud LeftEnd::" + Mathf.Min (Lines.Select ((e) => e.LeftEnd).ToArray()).ToString());
+			return Mathf.Min (Lines.Select ((e) => e.LeftEnd).ToArray());
+		}
+	}
+
+	private List<Line> lines = new List<Line>(5);
+
+	void Awake () {
+		this.lines.Add(transform.Find ("1st").GetComponent<Line>());
+		this.lines.Add(transform.Find ("2nd").GetComponent<Line>());
+		this.lines.Add(transform.Find ("3rd").GetComponent<Line>());
+		this.lines.Add(transform.Find ("4th").GetComponent<Line>());
+		this.lines.Add(transform.Find ("5th").GetComponent<Line>());
+
+		SetLinesInitialPosition();
+
+		// create enemies
+		Enumerable
+			.Range (0, this.Lines.Count)
+			.Select ((i) => this.lines[i].CreateEnemies())
+			.ToArray ();
+	}
+		
+	public IEnumerator MoveRight() {
+		for (int i = this.Lines.Count - 1; i >= 0; i--) {
+			this.lines[i].transform.position += new Vector3(MovingAmountX, 0);
+			yield return new WaitForSeconds (MovingIntervalPerLine);
+		}
+	}
+
+	public IEnumerator MoveLeft() {
+		for (int i = this.Lines.Count - 1; i >= 0; i--) {
+			this.lines[i].transform.position += new Vector3(-MovingAmountX, 0f);
+			yield return new WaitForSeconds (MovingIntervalPerLine);
+		}
+	}
+
+	public IEnumerator MoveDown() {
+		for (int i = this.Lines.Count - 1; i >= 0; i--) {
+			this.lines[i].transform.position += new Vector3(0f, -1f);
+			yield return new WaitForSeconds (MovingIntervalPerLine);
+		}
+	}
+
+	private void SetLinesInitialPosition() {
+		for (int i = 0; i < this.lines.Count; i++) {
+			this.lines[i].transform.position = new Vector3 (0f, FirstLineYPos - (i * 1.5f), 0f);
+		}
+	}
+}
---
+++ Assets/Resources/Script/Line.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * EnemyCloud内に存在する敵の列（1列）
+ */
+public class Line : MonoBehaviour {
+	// １列の敵の数
+	private static readonly int EnemyAmountPerLine = 11;
+	// 左右に隣り合う敵との距離
+	private static readonly float EnemyXSpace = 1.6f;
+	// 敵の初期位置をセンタリングするためのオフセット
+	private static readonly float OffsetX = EnemyXSpace * (EnemyAmountPerLine - 1) / 2;
+
+	private GameObject basicEnemyPrefab;
+	private List<GameObject> enemies = new List<GameObject>(11);
+
+	void Awake () {
+		basicEnemyPrefab = (GameObject)Resources.Load ("Prefab/Enemy");
+	}
+
+	public float RightEnd {
+		get { 
+			var myPos = this.transform.position.x;
+			var offset = Mathf.Max (enemies.Select ((e) => e.transform.position.x).ToArray());
+			return myPos + offset; 
+		}
+	}
+
+	public float LeftEnd {
+		get { 
+			var myPos = this.transform.position.x;
+			var offset = Mathf.Min (enemies.Select ((e) => e.transform.position.x).ToArray());
+			return myPos + offset; 
+		}
+	}
+
+	public IEnumerable<GameObject> CreateEnemies() {
+		for (int i = 0; i < EnemyAmountPerLine; i++) {
+			var position = new Vector3((float)(i * EnemyXSpace - OffsetX), 0f, 0f);
+			var obj = (GameObject)Instantiate (basicEnemyPrefab, position, Quaternion.identity);			
+			obj.transform.SetParent(this.transform, false);
+			enemies.Add (obj);
+		}
+		return enemies;
+	}
+}
