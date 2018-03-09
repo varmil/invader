4f7ffdd1604759a28fd78f8da9aac3c21437479b
Your Name <fkmks247@gmail.com>
2018-03-09 19:47:41 +0900

    Implement Dead effect

---
+++ Assets/Scripts/EnemyZone.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * 敵が動きうるゾーン全体
+ */
+public class EnemyZone : MonoBehaviour {
+	// 移動終了後から次の移動までの秒間隔
+	// TODO: speed up gradually
+	private static readonly float MovingInterval = .1f;
+
+	// Cloudの移動方向
+	enum MoveDirection : byte
+	{
+		Right,
+		Left,
+		Down
+	}
+
+	private EnemyCloud enemyCloud;
+
+	// 初期は右方向
+	private MoveDirection currentMoveDirection = MoveDirection.Right;
+	// 下方向へ移動した後の移動方向を決定するのに使用する
+	private MoveDirection previousMoveDirection;
+
+	void Awake () {
+		this.enemyCloud = transform.Find ("Lines").GetComponent<EnemyCloud> ();
+	}
+
+	void Start () {
+		StartCoroutine(StartCloudMoving());
+	}
+
+	private IEnumerator StartCloudMoving() {
+		while (true) {
+			yield return new WaitForSeconds (MovingInterval);
+
+			// Look Line Position to judge move right or left or down
+			var nextDir = CalcNextMoveDirection();
+			switch (nextDir) {
+			case MoveDirection.Right:
+				yield return this.enemyCloud.MoveRight ();
+				break;
+			case MoveDirection.Left:
+				yield return this.enemyCloud.MoveLeft ();
+				break;
+			case MoveDirection.Down:
+				yield return this.enemyCloud.MoveDown ();
+				break;
+			}
+
+			// update state
+			this.previousMoveDirection = this.currentMoveDirection;
+			this.currentMoveDirection = nextDir;
+		}
+	}
+
+	private MoveDirection CalcNextMoveDirection() {
+		switch (this.currentMoveDirection) {
+		// 右端にCloudが達していたらDownへ
+		case MoveDirection.Right:
+			return (IsReachedRightEnd ()) 
+				? MoveDirection.Down 
+				: MoveDirection.Right;
+		// 左端にCloudが達していたらDownへ
+		case MoveDirection.Left:
+			return (IsReachedLeftEnd ()) 
+				? MoveDirection.Down 
+				: MoveDirection.Left;
+		// 一つ前のMoveDirectionを参照して、それと逆方向へ
+		case MoveDirection.Down:
+			return (this.previousMoveDirection == MoveDirection.Right)
+				? MoveDirection.Left
+				: MoveDirection.Right;
+		default:
+			throw new InvalidOperationException("cannot set MoveDirection");
+		}
+	}
+
+	private bool IsReachedRightEnd() {
+		return this.enemyCloud.RightEnd >= Constants.Stage.RightEnd;
+	}
+
+	private bool IsReachedLeftEnd() {
+		return this.enemyCloud.LeftEnd <= Constants.Stage.LeftEnd;
+	}
+}
---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * EnemyCloud内に存在する敵の列（1列）
+ */
+public class Line : MonoBehaviour {
+	// １列の敵の数
+	private static readonly int EnemyAmountPerLine = 11;
+	// 左右に隣り合う敵との距離
+	private static readonly float EnemyXSpace = 1.6f;
+	// 敵の初期位置をセンタリングするためのオフセット
+	private static readonly float OffsetX = EnemyXSpace * (EnemyAmountPerLine - 1) / 2;
+
+	public List<Enemy> Enemies {
+		get { return enemies; }
+	}
+
+	public IEnumerable<Enemy> AliveEnemies {
+		get { return Enemies.Where (e => e.Alive); }
+	}
+
+	private List<Enemy> enemies = new List<Enemy>(11);
+
+	private GameObject basicEnemyPrefab;
+
+	void Awake () {
+		basicEnemyPrefab = (GameObject)Resources.Load ("Prefabs/Enemy");
+	}
+
+	public float RightEnd {
+		get { 
+			var myPos = this.transform.position.x;
+			var offset = Mathf.Max (AliveEnemies.Select ((e) => e.transform.localPosition.x).ToArray());
+			return myPos + offset; 
+		}
+	}
+
+	public float LeftEnd {
+		get { 
+			var myPos = this.transform.position.x;
+			var offset = Mathf.Min (AliveEnemies.Select ((e) => e.transform.localPosition.x).ToArray());
+			return myPos + offset; 
+		}
+	}
+
+	public IEnumerable<Enemy> CreateEnemies() {
+		for (int i = 0; i < EnemyAmountPerLine; i++) {
+			var position = new Vector3((float)(i * EnemyXSpace - OffsetX), 0f, 0f);
+			var obj = (GameObject)Instantiate (basicEnemyPrefab, position, Quaternion.identity);
+			obj.transform.SetParent(this.transform, false);
+
+			var enemy = obj.GetComponent<Enemy> ();
+			enemy.Id = this.name + "-enemy::" + i;
+			enemies.Add (enemy);
+		}
+		return enemies;
+	}
+}
